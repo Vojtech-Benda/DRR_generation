f4be7774bc2647910fc825bfe4ac9cd77a0618da
Vojtech-Benda <126555118+Vojtech-Benda@users.noreply.github.com>
2023-08-10 16:38:15 +0200

    Update drrGen.py

--- drrGen.py
+++ drrGen.py
@@ -7,7 +7,7 @@ input_name = "D:\\skola\\DP\\dp_isr\\sample_files\\dicom\\"
 # rotation angles of transformation (degrees)
 rx = -90.0
 ry = 0.0
-rz = 90.0
+rz = 0.0
 
 # translation values of transformation (mm)
 tx = 0.0
@@ -28,9 +28,14 @@ spd = 595.0  # source to patient distance
 dx = 512.0
 dy = 779.0
 
+# output DRR image origin location
 # o2Dx = 0.0
 # o2Dy = 0.0
-threshold = 0
+
+# Hounsfield values below this threshold not taken into
+# account during X-ray simulation
+# threshold of 200 reasonably omits soft tissue
+threshold = 200
 
 dimension = 3
 
@@ -59,14 +64,12 @@ metadata = dicom_io.GetMetaDataDictionary()
 print(f"region: {image.GetBufferedRegion()}, resolution: {image.GetSpacing()}, origin: {image.GetOrigin()}, "
       f"type: {type(image)}")
 
-# setting the volume origin to the volume center
-# image_center = image.GetBufferedRegion().GetSize()
-# image_center[0] //= 2
-# image_center[1] //= 2
-# image_center[2] //= 2
-# image_center_mm = image.TransformIndexToPhysicalPoint(image_center)
-# image.SetOrigin(image_center_mm)
-# image.GetOrigin()
+# getting center of input volume to rotate x-ray - DRR image ("detector") around
+im_center = image.GetBufferedRegion().GetSize()
+im_center[0] //= 2
+im_center[1] //= 2
+im_center[2] //= 2 # index values in pixels
+im_center_mm = image.TransformIndexToPhysicalPoint(im_center) # output in milimeters
 
 # filter for the output DRR image
 resample_filter = itk.ResampleImageFilter[input_image_type, input_image_type].New()
@@ -75,7 +78,6 @@ resample_filter.SetDefaultPixelValue(0)
 
 # transform for the x-ray source and DRR image ("detector")
 transform = itk.CenteredEuler3DTransform[itk.D].New()
-# transform = transfor_type.New()
 transform.SetComputeZYX(True)
 translation = [tx, ty, tz]
 
@@ -86,10 +88,10 @@ im_resolution = image.GetSpacing()
 im_region = image.GetBufferedRegion()
 im_size = im_region.GetSize()
 
-im_origin[0] += im_resolution[0] * im_size[0] / 2.0
-im_origin[1] += im_resolution[1] * im_size[1] / 2.0
-im_origin[2] += im_resolution[2] * im_size[2] / 2.0
-center = [cx + im_origin[0], cy + im_origin[1], cz + im_origin[2]]
+# im_origin[0] += im_resolution[0] * im_size[0] / 2.0
+# im_origin[1] += im_resolution[1] * im_size[1] / 2.0
+# im_origin[2] += im_resolution[2] * im_size[2] / 2.0
+center = [cx + im_center[0], cy + im_center[1], cz + im_center[2]]
 transform.SetCenter(center)
 print(f"Image Size: {im_size}\n"
       f"Resolution: {im_resolution}\n"
@@ -102,7 +104,8 @@ interpolator = itk.itkRayCastInterpolateImageFunctionPython.itkRayCastInterpolat
 interpolator.SetTransform(transform)
 interpolator.SetThreshold(threshold)
 
-# focal point's location in x and y-axis same as volume origin
+# focal point's location in x and y-axis same as volume origin,
+# moved away from input volume along z-axis by source to patient distance
 focal_point = [im_origin[0],
                im_origin[1],
                im_origin[2] - spd]
@@ -117,19 +120,16 @@ size = itk.Size[dimension]((int(dx), int(dy), 1))
 resample_filter.SetSize(size)
 
 # output DRR image spacing same as volume's spacing and slice thickness
-spacing = [im_resolution[0], im_resolution[2], 1.0] # [0.935574, 0.7, 1.0]
+spacing = [im_resolution[0], im_resolution[2], 1.0]
 resample_filter.SetOutputSpacing(spacing)
 
 print(f"Output image size: {size}\n"
       f"Output image spacing: {spacing}")
 
 # setting output DRR image location
-# x-axis: DRR image origin same as focal point origin
-# y-axis: subtraction by output DRR image height (mm)
-# aligns it to the bottom of CT volume
-# z-axis: volume origin + source to image (detector) distance
+# z-axis: distance between x-ray source and DRR image is source to detector distance
 origin = [focal_point[0],
-          focal_point[1] - (im_resolution[2] * dy),
+          focal_point[1],
           focal_point[2] + sid]
 
 resample_filter.SetOutputOrigin(origin)
@@ -146,6 +146,7 @@ cast_image_filter = itk.CastImageFilter[input_image_type, output_image_type].New
 cast_image_filter.SetInput(rescaler.GetOutput())
 cast_image_filter.Update()
 
+# displaying the result
 image_view = itk.GetArrayViewFromImage(cast_image_filter.GetOutput())
 
 plt.imshow(image_view[0], cmap="gray")
